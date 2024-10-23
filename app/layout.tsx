a43e1f2085e3ef182dd4adc63e6852c4081c3f9c
Marwen Cherif <m.cherif@revers.io>
2024-10-23 05:44:43 +0100

    Upgrade next 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
